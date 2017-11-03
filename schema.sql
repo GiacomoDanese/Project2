6eff59b449d35861acf2decd01716c0077503173
Giacomo Danese <Giacomo@ZingTasks-MacBook-Pro.local>
2017-11-02 20:45:03 -0700

    added directories

---
+++ schema.sql
@@ -0,0 +1,18 @@
+-- Looks to see if a database exists with that name. Deletes it and creates a new dtabase
+DROP DATABASE IF EXISTS store_db;
+CREATE DATABASE store_db;
+--Select the data base.
+-- USE store_db;
+
+-- CREATE TABLE components
+-- (
+-- id INT NOT NULL AUTO_INCREMENT,
+-- item_name VARCHAR(255) NOT NULL,
+-- item_type VARCHAR(255) NOT NULL,
+-- item_img VARCHAR(255) NOT NULL,
+-- item_tier INT(100) NOT NULL,
+-- item_price INT (18,2) NOT NULL,
+-- item_stock INT(100) NOT NULL
+-- PRIMARY KEY(id)
+-- );
+
