eb247e03fcd50ba0335397a62cd7fb0409e8ebf1
Giacomo Danese <Giacomo@ZingTasks-MacBook-Pro.local>
2017-11-08 20:02:29 -0700

    stuff

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,11 +6,61 @@
 var db = require("../models");
 
 module.exports = function(app) {
-	app.get("/api", function(req, res) {
-		db.Components.findAll({}).then(function(data) {
+	app.get("/api/worm", function(req, res) {
+		db.Components.findAll({
+			where: {
+				item_tier: 1
+			}
+		}).then(function(data) {
 			res.render("index", {stuff:data});
 
 			console.log(data);
 		})
-	})
+	});
+
+
+
+	app.get("/api/plant", function(req, res) {
+		db.Components.findAll({
+			where: {
+				item_tier: 2
+			}
+		}).then(function(data) {
+			res.render("index", {stuff:data});
+
+			console.log(data);
+		})
+	});
+
+
+
+	app.get("/api/human", function(req, res) {
+		db.Components.findAll({
+			where: {
+				item_tier: 3
+			}
+		}).then(function(data) {
+			res.render("index", {stuff:data});
+
+			console.log(data);
+		})
+	});
+
+
+
+	app.get("/api/zeus", function(req, res) {
+		db.Components.findAll({
+			where: {
+				item_tier: 4
+			}
+		}).then(function(data) {
+			res.render("index", {stuff:data});
+
+			console.log(data);
+		})
+	});
 }
+
+
+
+
